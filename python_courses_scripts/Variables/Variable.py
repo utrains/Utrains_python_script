1340fddee63bcf5251baeaf8266302930be69ffb
Kanaestephe <Kanaestephe@gmail.com>
2022-10-17 11:38:35 +0100

    Create Variable.py

---
+++ python_courses_scripts/Variables/Variable.py
@@ -0,0 +1,13 @@
+# A variable is a fundamental concept in any programming language. It is a reserved memory location that stores and manipulate data.
+
+# Let’s create some differents types of variables and then display their content.
+
+name="estephe" # this variable is used to store the name
+age="26"       # this variable is used to store the age
+weight="67.6"  # this variable is used to store the weight
+
+# Display the content of each above variables
+
+print('Your name is: ', name)
+print('Your age is: ', age)
+print('Your weight is: ', weight) 
