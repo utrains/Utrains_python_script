fe8ac85b6f391f04fe6ec3e0b0fe6b870af1138a
Kanaestephe <Kanaestephe@gmail.com>
2022-10-17 11:50:57 +0100

    Create helloworld.py

---
+++ python_courses_scripts/helloworld.py
@@ -0,0 +1,7 @@
+# Helloworld is the first script you should be able to write in every programming language
+
+# In this script, we display the simplest message: "Hello world" in the console
+
+# To display a message on a console/screen in python, we use print() function.
+
+print("Hello World")
