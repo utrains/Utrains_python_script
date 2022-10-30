14723bf4c86da611b8a42d897897341d12bf9d29
Kanaestephe <Kanaestephe@gmail.com>
2022-10-30 23:57:44 +0100

    Update python_numbers.py

--- python_courses_scripts/Data types/python_numbers.py
+++ python_courses_scripts/Data types/python_numbers.py
@@ -3,11 +3,11 @@
 ### Create an integer and get its type and value
 
 age = 26 # create an integer with the value 26
-print(age) # print() function is used display the value of age
+print('The age is: ', age) # print() function is used display the value of age
 print('The type of the variable age is: ', type(age)) # type() function is used to display the type of variable age
 
 ### Create a float and get its type and value
 
 weight = 67.6 # create a float with the value 67.6
-print(weight) # display the value of weight
+print('The weight is: ', weight) # display the value of weight
 print('The type of the variable weight is: ', type(weight)) # display the type of variable weight
