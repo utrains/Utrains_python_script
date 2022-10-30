36cb6892ab3687c243997b750e1d3fb5e306dc32
Kanaestephe <Kanaestephe@gmail.com>
2022-10-30 21:52:34 +0100

    Update and rename datatypes.py to python_numbers.py

---
+++ python_courses_scripts/Data types/python_numbers.py
@@ -0,0 +1,13 @@
+## The goal of this code is to show you how to define numbers in python
+
+### Create an integer and get its type and value
+
+age = 26 # create an integer with the value 26
+print(age) # print() function is used display the value of age
+print('The type of the variable age is: ', type(age)) # type() function is used to display the type of variable age
+
+### Create a float and get its type and value
+
+weight = 67.6 # create a float with the value 67.6
+print(weight) # display the value of weight
+print('The type of the variable weight is: ', type(weight)) # display the type of variable weight
