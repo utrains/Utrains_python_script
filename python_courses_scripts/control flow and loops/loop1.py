dde31d0caf621a87803eccf6e753cbf752d22e90
Kanaestephe <Kanaestephe@gmail.com>
2022-11-04 04:26:45 +0100

    Create loop1.py

---
+++ python_courses_scripts/control flow and loops/loop1.py
@@ -0,0 +1,8 @@
+# Assume you have a list of 5 numbers. Write a script to compute the square of each number using for loop.
+
+numbers = [1, 3, 5, 8, 12]
+for i in numbers:
+  square = i ** 2
+  print("Square of:", i, "is:", square)
+
+  
