2e98e3d42bca13a397990476ade1f95b5daa349e
Kanaestephe <Kanaestephe@gmail.com>
2022-11-04 04:15:14 +0100

    Create controlflow2.py

---
+++ python_courses_scripts/control flow and loops/controlflow2.py
@@ -0,0 +1,8 @@
+# Python script to take a number as an input from a user and then check if it is a positive number or not.
+
+number = input("Enter a number ")
+number = int(number)
+if number > 0:
+    print("It is a positive number ")
+else:
+    print("It is not a positive number ")
