49654db56fc5b6cbf79289eb025eb49cb26bfd55
Kanaestephe <Kanaestephe@gmail.com>
2022-11-04 04:12:42 +0100

    Create controlflow1.py

---
+++ python_courses_scripts/control flow and loops/controlflow1.py
@@ -0,0 +1,6 @@
+# Python script to take a number as an input from a user and then calculate the square of a number if it’s greater than 5.
+
+number = input("Enter a number ")
+number = int(number)
+if number > 5:
+print("The square of the entered number is: ", number * number)
