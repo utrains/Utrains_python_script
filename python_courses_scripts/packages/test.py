37a38c1a0aca31678d0d0df6d74fc13d98f7b09e
Kanaestephe <Kanaestephe@gmail.com>
2022-11-05 18:48:12 +0100

    Create test.py

---
+++ python_courses_scripts/packages/test.py
@@ -0,0 +1,8 @@
+# import the created package "Students"
+import Students
+
+# Display the list of IT field students
+print(Students.getITNames())
+
+# Display the list of None IT field students
+print(Students.getITNames())
