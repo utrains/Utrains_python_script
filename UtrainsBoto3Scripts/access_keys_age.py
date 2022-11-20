26872987d28dc9ad7d86b46c88f9b8b652b84662
Kanaestephe <Kanaestephe@gmail.com>
2022-11-20 07:08:16 +0100

    Update access_keys_age.py

--- UtrainsBoto3Scripts/access_keys_age.py
+++ UtrainsBoto3Scripts/access_keys_age.py
@@ -1,3 +1,4 @@
+#!/bin/python
 import boto3
 from datetime import date
 
@@ -7,5 +8,5 @@ response = client.list_access_keys(UserName=username)
 accesskeydate = response['AccessKeyMetadata'][0]['CreateDate'].date()
 currentdate = date.today()
 keys_age = currentdate - accesskeydate
-print (f"The access date of the key the user {username} is: {accesskeydate}")
+print (f"The access date key from the user {username} is: {accesskeydate}")
 print (f"The access key age is: {keys_age.days}")
