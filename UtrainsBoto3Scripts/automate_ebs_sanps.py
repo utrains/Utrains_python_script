dbd53ef37a0b48fab4553a2ef4afbf48567d975a
Kanaestephe <Kanaestephe@gmail.com>
2022-11-20 05:48:30 +0100

    Create automate_ebs_sanps.py

---
+++ UtrainsBoto3Scripts/automate_ebs_sanps.py
@@ -0,0 +1,43 @@
+#!/bin/python
+import boto3
+
+ec2_client=boto3.client("ec2")
+list_volumes_ids=[]
+
+filter_avail={'Name':'status', 'Values': ['available']}
+# get list of available Volumes Ids
+paginator = ec2_client.get_paginator('describe_volumes')
+for page in paginator.paginate(Filters=[filter_avail]):
+    for each_vol in page['Volumes']:
+        list_volumes_ids.append(each_vol['VolumeId'])
+
+print(f"The volumes Ids are: {list_volumes_ids}")
+
+# create a snapshot for each available volume
+snapshots_ids=[]
+for vol_id in list_volumes_ids:
+    print(f"Taking snap of {vol_id}")
+    response=ec2_client.create_snapshot( 
+             Description="Taking snap with Lambda and cw",
+             VolumeId=vol_id,
+             TagSpecifications=[
+                 {
+                    'ResourceType':'snapshot',
+                     'Tags': [
+                         {
+                            'Key': 'Delete-on',
+                            'Value': '90'
+                         }
+                             ]
+                 }
+                ]
+               )
+    snapshots_ids.append(response.get('SnapshotId'))
+
+print(f"The snapshots ids are: {snapshots_ids}")
+
+
+waiter = ec2_client.get_waiter('snapshot_completed')
+waiter.wait(SnapshotIds=snapshots_ids)
+
+print(f"Succssfully created snapshots for the volumes of {list_volumes_ids}")
